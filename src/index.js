7bd3f0ea75ad23eb828e6afca1b1f13c9685ce9f
Lilia Lomaka <llomaka80@gmail.com>
2022-02-23 10:11:41 +0200

    Додано Debounce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
+import { debounce } from "debounce";
 
+
+const inputRef = document.querySelector('#search-box');
 const DEBOUNCE_DELAY = 300;
+
+
+const onInput = event => {
+console.dir(event.target.value);
+//   if (email.value === "") {
+//     return alert("Поле Email формы должно быть заполнено!");
+//   }
+};
+
+inputRef.addEventListener('input', debounce(onInput, 300));
