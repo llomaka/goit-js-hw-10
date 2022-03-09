168ad8d3dbb03c08da5c890ad078ccaa120564ac
Lilia Lomaka <llomaka80@gmail.com>
2022-03-09 19:01:41 +0200

    Доданий приклад виконання від Репети

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,1 +1,7 @@
-export function fetchCountries(name) {}
+const BASE_URL = 'https://restcountries.com/v3.1';
+
+export function fetchCountries(name) {
+  return fetch(`${BASE_URL}/name/${name}`).then(response =>
+    response.json(),
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 import './css/styles.css';
 import { debounce } from "debounce";
 import { Notify } from 'notiflix';
-import { fetchCountries } from './fetchCountries';
+import getRefs from './getRefs';
+import fetchCountries from './fetchCountries';
 
-const refs = {
-  input: document.querySelector('#search-box'),
-  list: document.querySelector('.country-list'),
-  div: document.querySelector('.country-info'),
-}
+const refs = getRefs();
 const DEBOUNCE_DELAY = 300;
+
 refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
- Notify.init({
-    fontSize: '18px',
-  });
+
+Notify.init({ fontSize: '18px', });
 
 function onInput(event) {
+  event.preventDefault();
   const value = event.target.value.trim();
   if (!value) return Notify.warning('Please enter some text to input field!');
   console.log(value);
-
+  fetchCountries(value)
+    .then(renderList)
+    .catch(onSearchError)
+    .finally(() => event.reset());
 };
 
 function renderList(array) {
@@ -33,4 +34,8 @@ function renderInfo(array) {
   refs.list.innerHTML = '';
   const markup = array[0](item => `<svg><use href="${item.path}"></use></svg><h0>${item}</h0><ul><li>${item}</li><li>${item}</li><li>${item}</li></ul>`);
   refs.list.insertAdjacentHTML('beforeend', markup);
+}
+
+function onSearchError(error) {
+  Nitify.failure('Oops, there is no country with that name');
 }
