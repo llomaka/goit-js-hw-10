83c86390a6b918413ead5688ec4ec2d33f31e922
Lilia Lomaka <llomaka80@gmail.com>
2022-03-16 12:32:58 +0200

    Налаштування виведення переліку та інформації

--- src/index.js
+++ src/index.js
@@ -15,26 +15,31 @@ function onInput(event) {
   event.preventDefault();
   const value = event.target.value.trim();
   if (!value) return Notify.warning('Please enter some text to input field!');
-  console.log(value);
   fetchCountries(value)
-    .then(renderList)
-    .catch(onSearchError);
-  event.reset();
+    .then(render)
+    .catch(onSearchError)
+    .finally(()=> event.reset());
 };
 
-function renderList(result) {
-  console.log(result);
+function render(array) {
+  console.log(array);
+  if (array.length > 10) { Notify.info('Too many matches found. Please enter a more specific name.'); }
+  else if (array.length > 1 && array.length < 10) { renderList(array); }
+  else if (array.length = 1) { renderInfo(array); }
+}
+
+function renderList(array) {
   refs.list.innerHTML = '';
   refs.div.innerHTML = '';
-  const markup = result.map(item => `<li><svg width="20" height="20"><use width="20" height="20" href="${item.flags.svg}"></use></svg> ${item.name.official}</li>`).join('');
+  const markup = array.map(item => `<li><img width="30" src="${item.flags.svg}" alt="Flag of ${item.name.common}"> ${item.name.official}</li>`).join('');
   refs.list.insertAdjacentHTML('beforeend', markup);
 }
 
 function renderInfo(array) {
   refs.div.innerHTML = '';
   refs.list.innerHTML = '';
-  const markup = array[0](item => `<svg><use href="${item.path}"></use></svg><h0>${item}</h0><ul><li>${item}</li><li>${item}</li><li>${item}</li></ul>`);
-  refs.list.insertAdjacentHTML('beforeend', markup);
+  const markup = array.map(item => `<h1><img width="60" src="${item.flags.svg}">${item.name.official}</h1><ul><li><h2>Capital:</h2> ${item.capital}</li><li><h2>Population:</h2> ${item.population.toLocaleString()}</li><li><h2>Languages:</h2> ${Object.values(item.languages).join(", ")}</li></ul>`);
+  refs.div.insertAdjacentHTML('beforeend', markup);
 }
 
 function onSearchError(error) {
