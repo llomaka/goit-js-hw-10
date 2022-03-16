d5fa6b041f5075facf12ab476ed34e3c0bdad259
Lilia Lomaka <llomaka80@gmail.com>
2022-03-16 19:09:36 +0200

    Додавання обробки помилки fetchCountry, переналаштування виведення для роботи з версією api 2

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'https://restcountries.com/v3.1';
+const BASE_URL = 'https://restcountries.com/v2';
 
 export function fetchCountries(name) {
-  return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`).then(response => response.json());
+  return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`).then(response => response.json()).catch(error => console.log(error));
 }
--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ function onInput(event) {
 };
 
 function render(array) {
+  console.log(array);
   if (array.length > 10) { Notify.info('Too many matches found. Please enter a more specific name.'); }
   else if (array.length > 1 && array.length <= 10) { renderList(array); }
   else if (array.length = 1) { renderInfo(array); }
@@ -33,14 +34,14 @@ function render(array) {
 function renderList(array) {
   refs.list.innerHTML = '';
   refs.div.innerHTML = '';
-  const markup = array.map(item => `<li><img width="30" src="${item.flags.svg}" alt="Flag of ${item.name.common}"> ${item.name.official}</li>`).join('');
+  const markup = array.map(item => `<li><img width="30" src="${item.flags.svg}" alt="Flag of ${item.name}"> ${item.name}</li>`).join('');
   refs.list.insertAdjacentHTML('beforeend', markup);
 }
 
 function renderInfo(array) {
   refs.div.innerHTML = '';
   refs.list.innerHTML = '';
-  const markup = array.map(item => `<h1><img width="60" src="${item.flags.svg}">${item.name.official}</h1><ul><li><h2>Capital:</h2> ${item.capital.join(", ")}</li><li><h2>Population:</h2> ${item.population.toLocaleString()}</li><li><h2>Languages:</h2> ${Object.values(item.languages).join(", ")}</li></ul>`);
+  const markup = array.map(item => `<h1><img width="60" src="${item.flags.svg}">${item.name}</h1><ul><li><h2>Capital:</h2> ${item.capital}</li><li><h2>Population:</h2> ${item.population.toLocaleString()}</li><li><h2>Languages:</h2> ${(item.languages.map(language => language.name).join(", "))}</li></ul>`);
   refs.div.insertAdjacentHTML('beforeend', markup);
 }
 
